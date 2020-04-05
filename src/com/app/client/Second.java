e3644d4a330b40ed8f813c857f4e794144fe22fb
HOME <HOME@HOME-PC>
2020-04-05 20:46:44 +0530

    Third file

---
+++ src/com/app/client/Second.java
@@ -0,0 +1,19 @@
+package com.app.client;
+
+public class Second {
+private int stdName;
+
+public Second(int stdName) {
+	super();
+	this.stdName = stdName;
+}
+
+public int getStdName() {
+	return stdName;
+}
+
+public void setStdName(int stdName) {
+	this.stdName = stdName;
+}
+
+}
