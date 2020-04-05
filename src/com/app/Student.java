72ef379fdeb9c6f3e10910670f462254b91ca3af
HOME <HOME@HOME-PC>
2020-04-05 20:34:42 +0530

    Second File

---
+++ src/com/app/Student.java
@@ -0,0 +1,19 @@
+package com.app;
+
+public class Student {
+private int stdId;
+
+public Student(int stdId) {
+	super();
+	this.stdId = stdId;
+}
+
+public int getStdId() {
+	return stdId;
+}
+
+public void setStdId(int stdId) {
+	this.stdId = stdId;
+}
+
+}
